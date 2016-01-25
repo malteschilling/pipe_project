de7e234670b027b63ac396779cd317c2af94fda7
kbuschme <kbuschme@techfak.uni-bielefeld.de>
2016-01-25 20:03:55 +0100

    Add lane extension to handle multiple lanes

---
+++ src/main/java/traffic_sim/LaneExtension.java
@@ -0,0 +1,116 @@
+package traffic_sim;
+
+import java.awt.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LaneExtension implements VehicleConsumerInterface,
+									  VehicleProducerInterface {
+	// All positions are the same right now.
+	// Better: change VehicleProducerInterface and VehicleConsumerInterface
+	// to prohibit setting of start/end points.
+	private Point position, startPosition, endPosition;
+	private List<Lane> outgoingLanes;
+	private Lane endLane;
+
+	public LaneExtension(Point position) {
+		setPositions(position);
+		outgoingLanes = new ArrayList<Lane>();
+	}
+
+	public LaneExtension(int xPosition, int yPosition) {
+		setPositions(new Point(xPosition, yPosition));
+		outgoingLanes = new ArrayList<Lane>();
+	}
+
+	public LaneExtension(Point position, List<Lane> outgoingLanes,
+			Lane endLane) {
+		setPositions(position);
+
+		this.outgoingLanes = outgoingLanes;
+		this.endLane = endLane;
+	}
+
+	public LaneExtension(int xPosition, int yPosition,
+			List<Lane> outgoingLanes, Lane endLane) {
+		setPositions(new Point(xPosition, yPosition));
+
+		this.outgoingLanes = outgoingLanes;
+		this.endLane = endLane;
+	}
+
+	private void setPositions (Point position) {
+		this.position = position;
+		this.startPosition = position;
+		this.endPosition = position;
+	}
+
+	@Override
+	public boolean tryToConsumeVehicle(Vehicle veh) {
+		int numberOfOutgoingLanes = outgoingLanes.size();
+		if (numberOfOutgoingLanes > 0) {
+			veh.switchToLane(outgoingLanes);
+		} else {
+			veh.removeVehicleFromLane();
+		}
+//		veh.removeVehicleFromLane();
+		return true;
+	}
+
+	@Override
+	public void setLaneEnding(Lane lane) {
+		endLane = lane;
+	}
+
+	@Override
+	public Lane getLaneEnding() {
+		return endLane;
+	}
+
+	@Override
+	public Point getEndPoint() {
+		return endPosition;
+	}
+
+	@Override
+	public void setEndPoint(int x, int y) {
+		this.endPosition = new Point(x, y);
+	}
+
+	@Override
+	public void setLaneStarting(Lane lane) {
+		// Does not really set the lane but rather adds it to a list of possible
+		// lanes. Better: Change VehicleProducerInterface instead.
+		this.addOutgoingLanes(lane);
+	}
+
+	@Override
+	public Lane getLaneStarting() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Point getStartPoint() {
+		return startPosition;
+	}
+
+	@Override
+	public void setStartPoint(int x, int y) {
+		this.startPosition = new Point(x, y);
+	}
+
+	public List<Lane> getOutgoingLanes() {
+		return outgoingLanes;
+	}
+
+	public void setOutgoingLanes(List<Lane> incomingLanes) {
+		this.outgoingLanes = incomingLanes;
+	}
+
+	public void addOutgoingLanes(Lane ougoingLane) {
+		this.outgoingLanes.add(ougoingLane);
+	}
+
+}
--- src/main/java/traffic_sim/TrafficLight.java
+++ src/main/java/traffic_sim/TrafficLight.java
@@ -13,20 +13,20 @@ import java.util.ArrayList;
 public class TrafficLight implements VehicleProducerInterface, VehicleConsumerInterface,Drawable   {
 
     protected Lane lane_ends, lane_starts;
-	// The current state of the traffic light - right now only green (true) or 
+	// The current state of the traffic light - right now only green (true) or
 	// red (false).
     private boolean green = false;
-    
+
     // A list of all traffic lights - used for visualization.
     protected static ArrayList<TrafficLight> trafficLights = new ArrayList<>();
 
 	// Visualization points.
 	private Point start_point, end_point, graphic_pos;
-    
+
     public TrafficLight() {
     	trafficLights.add(this);
     }
-    
+
     /*
 	 * Counts number of cars directly in front of the traffic light.
 	 * Goes through the ending lane and tests if each spot in front of the light
@@ -45,12 +45,13 @@ public class TrafficLight implements VehicleProducerInterface, VehicleConsumerIn
 		}
 		return waiting;
     }
-	
+
 	/*
 	 * Overrides the VehicleConsumer method.
-	 * Checks if vehicles are allowed to pass and if so puts them 
+	 * Checks if vehicles are allowed to pass and if so puts them
 	 * on the lane leading away from the traffic light.
 	 */
+	@Override
 	public boolean tryToConsumeVehicle(Vehicle veh) {
 		if ((green) & (this.lane_starts.spaceForNewCarAvailable()) ) {
     		veh.switchToLane(this.lane_starts);
@@ -59,43 +60,51 @@ public class TrafficLight implements VehicleProducerInterface, VehicleConsumerIn
 	    	return false;
 	    }
     }
-    
+
+	@Override
 	public void setLaneEnding(Lane lane) {
 		lane_ends = lane;
 	}
-	
+
+	@Override
 	public Lane getLaneEnding() {
 		return lane_ends;
 	}
-	
+
+	@Override
 	public void setLaneStarting(Lane lane) {
 		this.lane_starts = lane;
 	}
-	
+
+	@Override
 	public Lane getLaneStarting() {
 		return this.lane_starts;
 	}
-	
+
 	/*
 	 * StartPoint, EndPoint are points used vor visualization
-	 * (where should the lanes be drawn) 
+	 * (where should the lanes be drawn)
 	 */
+	@Override
 	public Point getStartPoint() {
 		return start_point;
 	}
-	
+
+	@Override
 	public void setStartPoint(int x, int y) {
 		start_point = new Point(x, y);
 	}
-	
+
+	@Override
 	public Point getEndPoint() {
 		return end_point;
 	}
-	
+
+	@Override
 	public void setEndPoint(int x, int y) {
 		end_point = new Point(x, y);
 	}
-	
+
 	public void setTrafficLightGreen(boolean new_state) {
 		this.green = new_state;
 	}
@@ -103,15 +112,15 @@ public class TrafficLight implements VehicleProducerInterface, VehicleConsumerIn
 	public boolean isTrafficLightGreen() {
 		return this.green;
 	}
-	
+
 	/*
-	 * graphic_pos is a point used vor visualization
-	 * (where should the traffic light be drawn) 
+	 * graphic_pos is a point used for visualization
+	 * (where should the traffic light be drawn)
 	 */
 	public Point getTrafficLightPosition() {
 		return this.graphic_pos;
-	} 
-	
+	}
+
 	public void setTrafficLightPosition(int x, int y) {
 		this.graphic_pos = new Point(x, y);
 	}
