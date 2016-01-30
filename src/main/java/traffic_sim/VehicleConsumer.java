0b75ebc0dea1f965986afce139914ff6d10b9024
kbuschme <kbuschme@techfak.uni-bielefeld.de>
2016-01-30 14:40:38 +0100

    Add constructors with element's position as argument
    Add additional (convenient) constructors to the VehicleConsumer and
    VehicleProducer classes that take coordinates or Points as arguments
    which specify the element's position.

--- src/main/java/traffic_sim/VehicleConsumer.java
+++ src/main/java/traffic_sim/VehicleConsumer.java
@@ -4,7 +4,7 @@ import java.awt.Point;
 
 /**
  * VehicleConsumer
- * 
+ *
  * Generic implementation of the VehicleConsumerInterface.
  * VehicleConsumer are endpoints of lanes, in this case
  * cars simply are removed from the lane and disappear.
@@ -15,41 +15,54 @@ public class VehicleConsumer implements VehicleConsumerInterface {
     protected Lane lane_ends;
     // Point for graphic visualization
     private Point end_point;
-    
+
     public VehicleConsumer() {
     	end_point = new Point();
     }
-    
+
+    public VehicleConsumer(Point position) {
+    	end_point = position;
+    }
+
+    public VehicleConsumer(int xCoordinate, int yCoordinate) {
+    	end_point = new Point(xCoordinate, yCoordinate);
+    }
+
     /*
      * A VehicleConsumer has to implement this method to check
-     * if vehicles are allowed to leave a lane (e.g. for a traffic light this 
+     * if vehicles are allowed to leave a lane (e.g. for a traffic light this
      * is not always the case).
      */
-    public boolean tryToConsumeVehicle(Vehicle veh) {
-    	veh.removeVehicleFromLane();	
+    @Override
+	public boolean tryToConsumeVehicle(Vehicle veh) {
+    	veh.removeVehicleFromLane();
     	return true;
     }
-    
+
     /*
      * Set connected lane.
      */
+	@Override
 	public void setLaneEnding(Lane lane) {
 		lane_ends = lane;
 	}
-	
+
 	/*
 	 * Get connected lane.
 	 */
+	@Override
 	public Lane getLaneEnding() {
 		return lane_ends;
 	}
-	
+
+	@Override
 	public Point getEndPoint() {
 		return end_point;
 	}
-	
+
+	@Override
 	public void setEndPoint(int x, int y) {
 		end_point = new Point(x, y);
 	}
-    
+
 }
--- src/main/java/traffic_sim/VehicleProducer.java
+++ src/main/java/traffic_sim/VehicleProducer.java
@@ -4,10 +4,10 @@ import java.awt.*;
 
 /**
  * VehicleProducer
- * 
+ *
  * Generic implementation of the VehicleProducerInterface.
  * VehicleProducer are startpoints of lanes, in this case
- * cars simply are appearing every couple of time steps - therefore it is a 
+ * cars simply are appearing every couple of time steps - therefore it is a
  * TemporalTrafficObject which are all called in simulation update steps.
  */
 public class VehicleProducer extends TemporalTrafficObject implements VehicleProducerInterface {
@@ -17,16 +17,27 @@ public class VehicleProducer extends TemporalTrafficObject implements VehiclePro
     private double time = 0.;
     // Point for graphic visualization
     private Point start_point;
-    
+
     public VehicleProducer() {
     	super();
     	start_point = new Point();
     }
-    
+
+    public VehicleProducer(Point position) {
+    	super();
+    	this.start_point = position;
+    }
+
+    public VehicleProducer(int xCoordiante, int yCoordinate) {
+    	super();
+    	start_point = new Point(xCoordiante, yCoordinate);
+    }
+
     /*
      * Update Step of the simulation.
      */
-    public void updateStep(double duration) {
+    @Override
+	public void updateStep(double duration) {
 		if (time % 5 == 0.) {
 			if (this.lane_starts.spaceForNewCarAvailable() ) {
 				new Vehicle( ("Car_" + time), this.lane_starts );
@@ -35,20 +46,24 @@ public class VehicleProducer extends TemporalTrafficObject implements VehiclePro
 		time += duration;
 	}
 
+	@Override
 	public void setLaneStarting(Lane lane) {
 		this.lane_starts = lane;
 	}
-	
+
+	@Override
 	public Lane getLaneStarting() {
 		return this.lane_starts;
 	}
-	
+
+	@Override
 	public Point getStartPoint() {
 		return start_point;
 	}
-	
+
+	@Override
 	public void setStartPoint(int x, int y) {
 		start_point = new Point(x, y);
 	}
-   
+
 }
