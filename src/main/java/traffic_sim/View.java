55e11e14eea4827a6c54c0996ab2179c9afdefbe
kbuschme <kbuschme@techfak.uni-bielefeld.de>
2016-01-30 14:38:10 +0100

    Make content of the view visible after lauch
    Make the content of the view visible after the window appears
    without requiring the window to be resized.

--- src/main/java/traffic_sim/View.java
+++ src/main/java/traffic_sim/View.java
@@ -18,14 +18,14 @@ public class View {
         frame.getContentPane().setLayout(new BorderLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800,400);
-        frame.setVisible(true);
 
         this.trafficView = new RoadPane();
-
 
         frame.getContentPane().add(trafficView, BorderLayout.CENTER);
         button = new JButton("EXIT");
         frame.getContentPane().add(button, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
     }
 
     public JButton getButton(){
