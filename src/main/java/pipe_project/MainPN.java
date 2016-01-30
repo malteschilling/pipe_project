72e933c641119298286cda1e58793c0dc1ac93b7
kbuschme <kbuschme@techfak.uni-bielefeld.de>
2016-01-30 13:09:39 +0100

    Refactor main method to call scene creating methods
    Refactor the main method, such that all traffic scences are contained
    in methods. The following three scenes exist:
    
    * Example scenario with two lanes and one traffic light
    * Example scenario extended with one building and one additional lane
    * Scene containing an intersection with additional turn left/right lanes

--- src/main/java/pipe_project/MainPN.java
+++ src/main/java/pipe_project/MainPN.java
@@ -51,37 +51,147 @@ public class MainPN {
 
 	public static RealTimePetriNetRunner runner;
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
      	// 1. Construct main window showing traffic situation (and exit button)
 		final TrafficController controller = new TrafficController();
 
-        // 2. Construct traffic simulation scene
+    	// 2. Construct traffic simulation scene
+		TrafficLight tl = createTwoLaneIntersection(controller);
+
+
+		// An object observing the simulator state (is pulled each simulation update)
+        // and can change the marking in the Petri Network (in the target place)
+        TrafficLightObserver tlWaiting = new TrafficLightObserver(tl);
+        tlWaiting.setActionPNTargetPlace("WAITING");
+
+        try {
+        	// 3. Build the simple Petri network controlling traffic:
+        	// when there are 3 or more cars in front of the traffic light
+        	// (this is tested in the TrafficLightObserver above)
+        	// the PetriNet place for WAITING is marked which causes
+        	// the following transition to fire.
+        	// This external transition is linked to an external action which switches
+        	// the traffic light (this seems overly complicated in this trivial scenario
+        	// but becomes important when one has to deal with many concurrent ongoing
+        	// and possibly temporal processes depending on the petri net).
+        	PetriNet currentPN = buildNet(tl);
+        	Collection<Place> placesPN = (currentPN).getPlaces();
+	    	for (Place singlePlaceObj : placesPN) {
+	    		System.out.println("Places: " + singlePlaceObj.getId() + " = "
+	    				+ singlePlaceObj.getNumberOfTokensStored() );
+   			}
+
+	    	// Start running of the PN
+    		runner = new RealTimePetriNetRunner( currentPN );
+			FiringGenericActionListener firedTrans = new FiringGenericActionListener();
+			runner.addPropertyChangeListener(firedTrans);
+
+			runner.startRealTimeClock();
+
+			// Visualization of the PN
+			/*VisualizationPetriNetFrame frame = new VisualizationPetriNetFrame( currentPN );
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setSize(640, 480);
+			frame.setVisible(true);*/
+			controller.control(runner);
+    	} catch (Exception e) {
+	    		e.printStackTrace();
+    	}
+
+    }
+
+
+    /**
+     * Create a simple traffic scene that consists of one road and
+     * one traffic light.
+     *
+     * This methods serves as a simple example of how a traffic scene is build.
+     *
+     * At the beginning of the road a VehicleProducer sets cars onto the road.
+     * At the end of the road a VehicleConsumer takes the cars from the road.
+     * The road consists of two lanes. One lane before the traffic light and
+     * one lane after the traffic light. The TrafficLight object itself thus
+     * connects the two lanes, which comprise the road.
+     *
+     * @param controller
+     * @return The contained traffic light
+     */
+    private static TrafficLight createTrafficLightSceneExample(
+    		TrafficController controller) {
+
 		// for example: on the left side a VehicleProducer sets vehicles onto the first lane
         VehicleProducer prod = new VehicleProducer();
         // has to define a position for graphic layout in visualization
         prod.setStartPoint(100, 100);
         // on right hand: vehicles disappear in this VehicleConsumer
         VehicleConsumer cons = new VehicleConsumer();
-        cons.setEndPoint(500, 500);
+        cons.setEndPoint(500, 100);
         // Traffic light in the middle (shown at end of first lane)
-		Building building = new Building(500, 100, "testPoint");
-		TrafficLight tl = new TrafficLight();
+
+
+        // Create a traffic light
+		TrafficLight trafficLight = new TrafficLight();
         // again, points have to be defined for the visualization in order to define where
         // the lane should be drawn
+		trafficLight.setStartPoint(310, 100);
+		trafficLight.setEndPoint(300, 100);
+        // and a position for the traffic light itself has to be defined
+		trafficLight.setTrafficLightPosition( 290, 120);
+
+		// The lanes connecting the different producers-consumers
+        Lane laneBeforeTrafficLight = new Lane("VorAmpel", prod, trafficLight);
+		Lane laneAfterTrafficLight = new Lane("NachBuilding", trafficLight, cons);
+
+		controller.getView().getTrafficView().addAllDrawables(trafficLight,
+			laneBeforeTrafficLight, laneAfterTrafficLight);
+
+		return trafficLight;
+    }
+
+
+    /**
+     * Create a traffic simulation scene that consists of two roads,
+     * a traffic light and a building.
+     *
+     * @param controller
+     * @return The traffic light object in the scene
+     */
+    private static TrafficLight createTrafficLightSceneWithBuilding(
+    		TrafficController controller) {
+
+        VehicleProducer prod = new VehicleProducer();
+        prod.setStartPoint(100, 100);
+
+        VehicleConsumer cons = new VehicleConsumer();
+        cons.setEndPoint(500, 500);
+
+		TrafficLight tl = new TrafficLight();
         tl.setStartPoint(310, 100);
         tl.setEndPoint(300, 100);
-        // and a position for the traffic light itself has to be defined
         tl.setTrafficLightPosition( 290, 120);
-        // The lanes connecting the different producers-consumers
+
+		Building building = new Building(500, 100, "testPoint");
+
         Lane lane_east = new Lane("VorAmpel", prod, tl);
         Lane lane_east_2 = new Lane("NachAmpel", tl, building);
 		Lane nachBuilding = new Lane("NachBuilding", building, cons);
 
-		controller.getView().getTrafficView().addAllDrawables(lane_east,lane_east_2,nachBuilding,tl,building);
+		controller.getView().getTrafficView().addAllDrawables(
+				lane_east, lane_east_2, nachBuilding, tl, building);
+
+		return tl;
+    }
 
 
-		// 2.1 - Lane extension before traffic light
+    /**
+     * Creates a traffic scene that consists of an intersection that consists
+     * of two extended lanes, i.e. the lane is extended by a turn left lane
+     * and a turn right lane.
+     * @param controller
+     * @return The traffic light object in the scene
+     */
+    private static TrafficLight createTwoLaneIntersection(
+    		TrafficController controller) {
 
 		// All lanes starting in the west
 		VehicleProducer westStart = new VehicleProducer();
@@ -105,9 +215,12 @@ public class MainPN {
 		westTurnNorthTL.setTrafficLightPosition(775, 265);
 
 		LaneExtension westTurnNorthExtension = new LaneExtension(750, 250);
-		Lane westTurnNorthLane1 = new Lane("TurnNorthLane1", extension, westTurnNorthExtension);
-		Lane westTurnNorthLane2 = new Lane("TurnNorthLane2", westTurnNorthExtension, westTurnNorthTL);
-		Lane westTurnNorthLane3 = new Lane("TurnNorthLane3", westTurnNorthTL, destination1);
+		Lane westTurnNorthLane1 = new Lane("TurnNorthLane1",
+				extension, westTurnNorthExtension);
+		Lane westTurnNorthLane2 = new Lane("TurnNorthLane2",
+				westTurnNorthExtension, westTurnNorthTL);
+		Lane westTurnNorthLane3 = new Lane("TurnNorthLane3",
+				westTurnNorthTL, destination1);
 
 		// Starting in the west, going straight
 		TrafficLight westStraightTL = new TrafficLight();
@@ -115,8 +228,10 @@ public class MainPN {
 		westStraightTL.setEndPoint(800, 300);
 		westStraightTL.setTrafficLightPosition(775, 315);
 
-		Lane westStraightBeforeTL = new Lane("GoStraightLane", extension, westStraightTL);
-		Lane westStraightAfterTL = new Lane("GoStraightLane", westStraightTL, destination2);
+		Lane westStraightBeforeTL = new Lane("GoStraightLane",
+				extension, westStraightTL);
+		Lane westStraightAfterTL = new Lane("GoStraightLane",
+				westStraightTL, destination2);
 
 		// Starting in the west, turning south
 		TrafficLight westTurnSouthTL = new TrafficLight();
@@ -125,9 +240,12 @@ public class MainPN {
 		westTurnSouthTL.setTrafficLightPosition(775, 365);
 
 		LaneExtension westTurnSouthExtension = new LaneExtension(750, 350);
-		Lane westTurnSouthLane1 = new Lane("TurnSouthLane1", extension, westTurnSouthExtension);
-		Lane westTurnSouthLane2 = new Lane("TurnSouthLane2", westTurnSouthExtension, westTurnSouthTL);
-		Lane westTurnSouthLane3 = new Lane("TurnSouthLane3", westTurnSouthTL, destination3);
+		Lane westTurnSouthLane1 = new Lane("TurnSouthLane1",
+				extension, westTurnSouthExtension);
+		Lane westTurnSouthLane2 = new Lane("TurnSouthLane2",
+				westTurnSouthExtension, westTurnSouthTL);
+		Lane westTurnSouthLane3 = new Lane("TurnSouthLane3",
+				westTurnSouthTL, destination3);
 
 
 		// All lanes starting in the EAST
@@ -190,12 +308,12 @@ public class MainPN {
 		northStart.setStartPoint(850, 50);
 		LaneExtension northExtension = new LaneExtension(850, 200);
 
-//		VehicleConsumer northDestination1 = new VehicleConsumer();
-//		northDestination1.setEndPoint(900, 20);
+    	// VehicleConsumer northDestination1 = new VehicleConsumer();
+    	// northDestination1.setEndPoint(900, 20);
 		VehicleConsumer northDestination2 = new VehicleConsumer();
 		northDestination2.setEndPoint(850, 500);
-//		VehicleConsumer northDestination3 = new VehicleConsumer();
-//		northDestination3.setEndPoint(900, 450);
+    	// VehicleConsumer northDestination3 = new VehicleConsumer();
+    	// northDestination3.setEndPoint(900, 450);
 
 		Lane northIncomingLane = new Lane("NorthIncoming",
 				northStart, northExtension);
@@ -239,53 +357,21 @@ public class MainPN {
 		westLeftTLOberserver.setActionPNTargetPlace("WAITING");
 
 
-		// An object observing the simulator state (is pulled each simulation update)
-        // and can change the marking in the Petri Network (in the target place)
-        TrafficLightObserver tlWaiting = new TrafficLightObserver( tl );
-        tlWaiting.setActionPNTargetPlace( "WAITING" );
 
-        try {
-        	// 3. Build the simple Petri network controlling traffic:
-        	// when there are 3 or more cars in front of the traffic light
-        	// (this is tested in the TrafficLightObserver above)
-        	// the PetriNet place for WAITING is marked which causes
-        	// the following transition to fire.
-        	// This external transition is linked to an external action which switches
-        	// the traffic light (this seems overly complicated in this trivial scenario
-        	// but becomes important when one has to deal with many concurrent ongoing
-        	// and possibly temporal processes depending on the petri net).
-        	PetriNet currentPN = buildNet( tl );
-        	Collection<Place> placesPN = ( currentPN ).getPlaces();
-	    	for (Place singlePlaceObj : placesPN) {
-	    		System.out.println("Places: " + singlePlaceObj.getId() + " = " + singlePlaceObj.getNumberOfTokensStored() );
-   			}
-
-	    	// Start running of the PN
-    		runner = new RealTimePetriNetRunner( currentPN );
-			FiringGenericActionListener firedTrans = new FiringGenericActionListener();
-			runner.addPropertyChangeListener(firedTrans);
-
-			runner.startRealTimeClock();
-
-			// Visualization of the PN
-			/*VisualizationPetriNetFrame frame = new VisualizationPetriNetFrame( currentPN );
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setSize(640, 480);
-			frame.setVisible(true);*/
-			controller.control(runner);
-    	} catch (Exception e) {
-	    		e.printStackTrace();
-    	}
+    	return westTurnNorthTL;
 
     }
+
 
     /**
      * Build test Petri network for traffic light.
      */
 	private static PetriNet buildNet(TrafficLight tl) throws PetriNetComponentException {
 		// Create a Petri Net.
-		PetriNet petriNet = APetriNet.with(AToken.called("Default").withColor(Color.BLACK)).and(APlace.withId("WAITING").externallyAccessible()).andFinally(
-    		APlace.withId("GREEN"));
+		PetriNet petriNet = APetriNet.with(AToken.called("Default").
+				withColor(Color.BLACK)).
+				and(APlace.withId("WAITING").externallyAccessible()).
+				andFinally(APlace.withId("GREEN"));
     	Collection<Place> placesPN = petriNet.getPlaces();
     	Place startPlace = null;
     	Place endPlace = null;
@@ -307,7 +393,9 @@ public class MainPN {
 		// (which I have built) and the external action objects which you
 		// can customize (see TrafficLightTransitionCall as an example).
     	TrafficLightTransitionCall extAct = new TrafficLightTransitionCall( tl );
-    	DiscreteExternalActionCallTransition newTransition = new DiscreteExternalActionCallTransition( "SwitchToGreen", "SwitchToGreen", extAct );
+    	DiscreteExternalActionCallTransition newTransition =
+    			new DiscreteExternalActionCallTransition(
+    					"SwitchToGreen", "SwitchToGreen", extAct );
     	petriNet.addTransition(newTransition);
 
 		// Connecting the network.
